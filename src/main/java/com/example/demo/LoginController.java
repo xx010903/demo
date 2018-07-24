7f3e8cba8057a547e1c8bb9eac97d0430dd9185d
xx010903 <455877588@qq.com>
2018-07-24 18:22:02 +0800

    login function

--- src/main/java/com/example/demo/LoginController.java
+++ src/main/java/com/example/demo/LoginController.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
+
+    @RequestMapping("login")
+    public void login(){
+
+    }
 }
